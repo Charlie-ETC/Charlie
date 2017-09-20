9ced258155c8d7306877d39468d21b8bbec62816
Wong Yong Jie <yjwong92@gmail.com>
2017-09-19 17:07:44 -0700

    Attempt at fixing "Plugin used in several locations".

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,10 +1,24 @@
+using System.Collections.Generic;
 using UnityEditor;
 
 public class Builder
 {
     public static void Build()
     {
-        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "UWP",
-            BuildTarget.WSAPlayer, BuildOptions.None);
+        List<string> sceneNames = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            sceneNames.Add(scene.path);
+        }
+
+        BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = sceneNames.ToArray(),
+            target = BuildTarget.WSAPlayer,
+            locationPathName = "UWP",
+            options = BuildOptions.None,
+            targetGroup = BuildTargetGroup.WSA
+        };
+        BuildPipeline.BuildPlayer(options);
     }
 }
