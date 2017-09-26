3236bb6eaedd9680f7ba289a7b8df70159f187ac
EvanWY <wangyang.evan@gmail.com>
2017-09-25 17:02:00 -0700

    add camera capture test

---
+++ Assets/_Scripts/HoloLens_Interaction/PhotoCaptureExample.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.VR.WSA.WebCam;
+
+public class PhotoCaptureExample : MonoBehaviour
+{
+    PhotoCapture photoCaptureObject = null;
+    public Texture2D targetTexture = null;
+    GameObject quad = null;
+
+
+    int cnt = 0;
+    private void Update()
+    {
+        cnt++;
+        if (cnt > 400)
+        {
+            cnt = 0;
+            Capture();
+        }
+    }
+
+    // Use this for initialization
+    void Capture()
+    {
+        Debug.Log("Capture");
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+
+        // Create a PhotoCapture object
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            // Activate the camera
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                // Take a picture
+                Debug.Log("photoCaptureObject.StartPhotoModeAsync");
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            });
+        });
+    }
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        Debug.Log("OnCapturedPhotoToMemory");
+        // Copy the raw image data into the target texture
+        photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+
+        // Create a GameObject to which the texture can be applied
+        if (quad != null)
+            GameObject.DestroyImmediate(quad);
+
+        quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
+        quadRenderer.material = new Material(Shader.Find("Unlit/Texture"));
+
+        quad.transform.parent = this.transform;
+        quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+
+        quadRenderer.material.SetTexture("_MainTex", targetTexture);
+
+        // Deactivate the camera
+        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+    }
+
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        Debug.Log("OnStoppedPhotoMode");
+        // Shutdown the photo capture resource
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
+}
