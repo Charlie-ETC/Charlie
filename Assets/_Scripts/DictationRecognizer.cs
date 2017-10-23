5f45f04b380ba1ec99e69ce2c77547d41f6317fb
Wong Yong Jie <yjwong92@gmail.com>
2017-10-23 04:12:05 -0700

    Namespaced Charlie.

--- Assets/_Scripts/DictationRecognizer.cs
+++ Assets/_Scripts/DictationRecognizer.cs
@@ -3,112 +3,117 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Windows.Speech;
 
-public class DictationRecognizer : MonoBehaviour {
-
-
-    [System.Serializable]
-    public class DictationCompleteEvent : UnityEvent<string> { }
-
-    [System.Serializable]
-    public class DictationErrorEvent : UnityEvent<string, int> { }
-
-    [System.Serializable]
-    public class DictationHypothesisEvent : UnityEvent<string> { }
-
-    [System.Serializable]
-    public class DictationResultEvent : UnityEvent<string, string> { }
-
-    private UnityEngine.Windows.Speech.DictationRecognizer dictationRecognizer;
-
-    public DictationCompleteEvent dictationCompleteEvent;
-    public DictationErrorEvent dictationErrorEvent;
-    public DictationHypothesisEvent dictationHypothesisEvent;
-    public DictationResultEvent dictationResultEvent;
-
-    [Tooltip("The time length in seconds before dictation recognizer session " +
-        "ends due to lack of audio input.")]
-    public float autoSilenceTimeoutSeconds = 20.0f;
-
-    [Tooltip("The time length in seconds before dictation recognizer session " +
-        "ends due to lack of audio input in case there was no audio heard in " +
-        "the current session.")]
-    public float initialSilenceTimeoutSeconds = 5.0f;
-
-    private bool isDestroying = false;
-
-	// Use this for initialization
-	void Start () {
-        dictationRecognizer = new UnityEngine.Windows.Speech.DictationRecognizer();
-        dictationRecognizer.DictationResult += OnDictationResult;
-        dictationRecognizer.DictationHypothesis += OnDictationHypothesis;
-        dictationRecognizer.DictationComplete += OnDictationComplete;
-        dictationRecognizer.DictationError += OnDictationError;
-        dictationRecognizer.AutoSilenceTimeoutSeconds = autoSilenceTimeoutSeconds;
-        dictationRecognizer.InitialSilenceTimeoutSeconds = initialSilenceTimeoutSeconds;
-        dictationRecognizer.Start();
-	}
-
-    public string FakeDictationResult;
-    public bool TriggerFakeDictationResult;
-
-    private void Update()
+namespace Charlie
+{
+    public class DictationRecognizer : MonoBehaviour
     {
-        if (TriggerFakeDictationResult)
-        {
-            OnDictationResult(FakeDictationResult, ConfidenceLevel.High);
-            TriggerFakeDictationResult = false;
-            FakeDictationResult = "";
-        }
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ResetDictationRecognizer();
-        }
-    }
 
-    private void OnDictationError(string error, int hresult)
-    {
-        Debug.Log($"[DictationRecognizer] OnDictationError (error: {error}, result: {hresult})");
-        dictationErrorEvent.Invoke(error, hresult);
-    }
+        [System.Serializable]
+        public class DictationCompleteEvent : UnityEvent<string> { }
 
-    private void OnDictationComplete(DictationCompletionCause cause)
-    {
-        Debug.Log($"[DictationRecognizer] OnDictationComplete (cause: {cause.ToString()})");
-        dictationCompleteEvent.Invoke(cause.ToString());
-        if (!isDestroying)
+        [System.Serializable]
+        public class DictationErrorEvent : UnityEvent<string, int> { }
+
+        [System.Serializable]
+        public class DictationHypothesisEvent : UnityEvent<string> { }
+
+        [System.Serializable]
+        public class DictationResultEvent : UnityEvent<string, string> { }
+
+        private UnityEngine.Windows.Speech.DictationRecognizer dictationRecognizer;
+
+        public DictationCompleteEvent dictationCompleteEvent;
+        public DictationErrorEvent dictationErrorEvent;
+        public DictationHypothesisEvent dictationHypothesisEvent;
+        public DictationResultEvent dictationResultEvent;
+
+        [Tooltip("The time length in seconds before dictation recognizer session " +
+            "ends due to lack of audio input.")]
+        public float autoSilenceTimeoutSeconds = 20.0f;
+
+        [Tooltip("The time length in seconds before dictation recognizer session " +
+            "ends due to lack of audio input in case there was no audio heard in " +
+            "the current session.")]
+        public float initialSilenceTimeoutSeconds = 5.0f;
+
+        private bool isDestroying = false;
+
+        // Use this for initialization
+        void Start()
         {
-            Debug.Log("[DictationRecognizer] Restarting DictationRecognizer after completion");
+            dictationRecognizer = new UnityEngine.Windows.Speech.DictationRecognizer();
+            dictationRecognizer.DictationResult += OnDictationResult;
+            dictationRecognizer.DictationHypothesis += OnDictationHypothesis;
+            dictationRecognizer.DictationComplete += OnDictationComplete;
+            dictationRecognizer.DictationError += OnDictationError;
+            dictationRecognizer.AutoSilenceTimeoutSeconds = autoSilenceTimeoutSeconds;
+            dictationRecognizer.InitialSilenceTimeoutSeconds = initialSilenceTimeoutSeconds;
             dictationRecognizer.Start();
         }
-    }
 
-    private void OnDictationHypothesis(string text)
-    {
-        Debug.Log($"[DictationRecognizer] OnDictationHypothesis (text: {text})");
-        dictationHypothesisEvent.Invoke(text);
-    }
+        public string FakeDictationResult;
+        public bool TriggerFakeDictationResult;
 
-    private void OnDictationResult(string text, ConfidenceLevel confidence)
-    {
-        Debug.Log($"[DictationRecognizer] OnDictationResult (text: {text}, confidence: {confidence.ToString()})");
-        dictationResultEvent.Invoke(text, confidence.ToString());
-        dictationRecognizer.Stop();
-    }
-
-    private void OnDestroy()
-    {
-        Debug.Log("[DictationRecognizer] Destroying DictationRecognizer");
-        if (dictationRecognizer.Status == SpeechSystemStatus.Running)
+        private void Update()
         {
-            isDestroying = true;
+            if (TriggerFakeDictationResult)
+            {
+                OnDictationResult(FakeDictationResult, ConfidenceLevel.High);
+                TriggerFakeDictationResult = false;
+                FakeDictationResult = "";
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ResetDictationRecognizer();
+            }
+        }
+
+        private void OnDictationError(string error, int hresult)
+        {
+            Debug.Log($"[DictationRecognizer] OnDictationError (error: {error}, result: {hresult})");
+            dictationErrorEvent.Invoke(error, hresult);
+        }
+
+        private void OnDictationComplete(DictationCompletionCause cause)
+        {
+            Debug.Log($"[DictationRecognizer] OnDictationComplete (cause: {cause.ToString()})");
+            dictationCompleteEvent.Invoke(cause.ToString());
+            if (!isDestroying)
+            {
+                Debug.Log("[DictationRecognizer] Restarting DictationRecognizer after completion");
+                dictationRecognizer.Start();
+            }
+        }
+
+        private void OnDictationHypothesis(string text)
+        {
+            Debug.Log($"[DictationRecognizer] OnDictationHypothesis (text: {text})");
+            dictationHypothesisEvent.Invoke(text);
+        }
+
+        private void OnDictationResult(string text, ConfidenceLevel confidence)
+        {
+            Debug.Log($"[DictationRecognizer] OnDictationResult (text: {text}, confidence: {confidence.ToString()})");
+            dictationResultEvent.Invoke(text, confidence.ToString());
             dictationRecognizer.Stop();
         }
-    }
 
-    public void ResetDictationRecognizer()
-    {
-        dictationRecognizer.Stop();
-        dictationRecognizer.Start();
+        private void OnDestroy()
+        {
+            Debug.Log("[DictationRecognizer] Destroying DictationRecognizer");
+            if (dictationRecognizer.Status == SpeechSystemStatus.Running)
+            {
+                isDestroying = true;
+                dictationRecognizer.Stop();
+            }
+        }
+
+        public void ResetDictationRecognizer()
+        {
+            dictationRecognizer.Stop();
+            dictationRecognizer.Start();
+        }
     }
 }
--- Assets/_Scripts/PlayMakerScripts/ActionResetDictation.cs
+++ Assets/_Scripts/PlayMakerScripts/ActionResetDictation.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using Asyncoroutine;
-
+using Charlie;
 
 public class ActionResetDictation : FsmStateAction
 {
