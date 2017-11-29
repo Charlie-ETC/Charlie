b718ae3430ae5c51000ac8b60c4276b7749a8cad
Angie <yangaq.10@hotmail.com>
2017-11-28 18:21:59 -0800

    optimize look at player

--- Assets/_Scripts/HoloLens_Interaction/LookAtPlayerIKControl.cs
+++ Assets/_Scripts/HoloLens_Interaction/LookAtPlayerIKControl.cs
@@ -11,15 +11,17 @@ public class LookAtPlayerIKControl : MonoBehaviour {
     private float angle;
     private const float NECK_ROTATION_RANGE = 80f;
     private const float ATTENTION_DISTANCE = 2F;
-    private const float TURN_BACK_SPEED_RATE = 0.99f;
+    private const float TURN_BACK_SPEED_RATE = 0.96f;
     private const float TURN_FORTH_SPEED_RATE = 0.1f;
     private float currentLookAtWeight;
 
+    private bool isTurningBody = false;
     private bool isLookingAway = false;
     private IEnumerator lookAwayCoroutine;
 
 
     public bool isActive;
+    public bool lookAwayOn;
     [Range(0,1)]
     public float lookAtWeight;
     [Range(0, 1)]
@@ -42,15 +44,15 @@ public class LookAtPlayerIKControl : MonoBehaviour {
 
     private IEnumerator LookAway() {
         while (true) {
-            if (isActive && !transform.parent.gameObject.GetComponent<AudioSource>().isPlaying) {
-                if (Random.Range(0f, 1f) < 0.2f) {
+            if (isActive && lookAwayOn && !transform.parent.gameObject.GetComponent<AudioSource>().isPlaying) {
+                if (Random.Range(0f, 1f) < 0.25f) {
                     isLookingAway = true;
-                    yield return new WaitForSeconds(Random.Range(1.5f, 5f));
+                    yield return new WaitForSeconds(Random.Range(1.5f, 2f));
                     isLookingAway = false;
                 }
             }
 
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(1.5f);
         }
         
     }
@@ -72,31 +74,46 @@ public class LookAtPlayerIKControl : MonoBehaviour {
                 // Debug.Log(charlieToPlayer.magnitude);
 
                 // when target is in Charlie's attention distance (too far to notice)
-                // and within her normal neck rotation range
-                // turn her head to face player
-                if (charlieToPlayer.magnitude < ATTENTION_DISTANCE && Mathf.Abs(angle) <= NECK_ROTATION_RANGE)
-                {
-                    //if (lookatPlayer.enabled) lookatPlayer.enabled = false;
+                
+                if (charlieToPlayer.magnitude < ATTENTION_DISTANCE) {
+                    // within her normal neck rotation range
+                    // turn her head to face player
+                    if (Mathf.Abs(angle) <= NECK_ROTATION_RANGE && !isTurningBody)
+                    {
+                        //if (lookatPlayer.enabled) lookatPlayer.enabled = false;
 
-                    if (lookAtWeight - currentLookAtWeight < 0.001f) { currentLookAtWeight = lookAtWeight; }
-                    else { 
-                        // currentLookAtWeight = Mathf.Lerp(currentLookAtWeight, lookAtWeight, TURN_FORTH_SPEED_RATE); 
-                        currentLookAtWeight += (1 - TURN_BACK_SPEED_RATE) * 0.8f;
+                        if (lookAtWeight - currentLookAtWeight < 0.001f) { currentLookAtWeight = lookAtWeight; }
+                        else
+                        {
+                            // currentLookAtWeight = Mathf.Lerp(currentLookAtWeight, lookAtWeight, TURN_FORTH_SPEED_RATE); 
+                            currentLookAtWeight += (1 - TURN_BACK_SPEED_RATE) * 0.8f;
+                        }
+                        //currentLookAtWeight = lookAtWeight;
+                        //Debug.Log(currentLookAtWeight);
+                        animator.SetLookAtWeight(currentLookAtWeight, bodyWeight, headWeight, eyeWeight);
+                        animator.SetLookAtPosition(target.transform.position);
                     }
-                    //currentLookAtWeight = lookAtWeight;
-                    //Debug.Log(currentLookAtWeight);
-                    animator.SetLookAtWeight(currentLookAtWeight, bodyWeight, headWeight, eyeWeight);       
-                    animator.SetLookAtPosition(target.transform.position);
-                }
-                else if (charlieToPlayer.magnitude < ATTENTION_DISTANCE)// otherwise turn around her body to face player
-                {
-                    if (currentLookAtWeight < 0.001f) { currentLookAtWeight = 0f; }
-                    else { currentLookAtWeight = Mathf.Lerp(0f, currentLookAtWeight, TURN_BACK_SPEED_RATE); }
-                    animator.SetLookAtWeight(currentLookAtWeight);
+                    else // otherwise turn around her body to face player
+                    {
+                        isTurningBody = true;
 
-                    float xZAngle = Vector3.Angle(Vector3.ProjectOnPlane(transform.parent.forward, Vector3.up), Vector3.ProjectOnPlane(charlieToPlayer, Vector3.up)) * (1f - TURN_BACK_SPEED_RATE) * 0.4f;
-                    transform.parent.eulerAngles = new Vector3(transform.parent.eulerAngles.x, transform.parent.eulerAngles.y + xZAngle, transform.parent.eulerAngles.z);
-                    //if (!lookatPlayer.enabled) lookatPlayer.enabled = true;
+                        if (currentLookAtWeight < 0.001f) { currentLookAtWeight = 0f; }
+                        else { currentLookAtWeight = Mathf.Lerp(0f, currentLookAtWeight, TURN_BACK_SPEED_RATE); }
+                        animator.SetLookAtWeight(currentLookAtWeight);
+
+                        Vector3 v1 = Vector3.ProjectOnPlane(transform.parent.forward, Vector3.up);
+                        Vector3 v2 = Vector3.ProjectOnPlane(charlieToPlayer, Vector3.up);
+                        float vAngle = Vector3.Angle(v1, v2);  // absolute value
+
+                        // use cross product to tell the direction
+                        if (Vector3.Cross(v1, v2).y < 0) vAngle = -vAngle;
+
+                        float xZAngle = vAngle * (1f - TURN_BACK_SPEED_RATE) * 0.4f;
+                        transform.parent.eulerAngles = new Vector3(transform.parent.eulerAngles.x, transform.parent.eulerAngles.y + xZAngle, transform.parent.eulerAngles.z);
+                        //if (!lookatPlayer.enabled) lookatPlayer.enabled = true;
+
+                        if (Mathf.Abs(vAngle) < 1f) isTurningBody = false;
+                    }
                 }
 
             }
--- Assets/_Scripts/PlayMakerScripts/ActionLookatPlayer.cs
+++ Assets/_Scripts/PlayMakerScripts/ActionLookatPlayer.cs
@@ -14,12 +14,14 @@ public class ActionLookatPlayer : FsmStateAction
 
     public FsmOwnerDefault objSelf;
     public bool lookAtPlayerOn;
+    public bool lookAway;
 
     public override void OnEnter()
     {
 
         // if we want to control her neck
         Fsm.GetOwnerDefaultTarget(objSelf).GetComponentInChildren<LookAtPlayerIKControl>().isActive = lookAtPlayerOn;
+        Fsm.GetOwnerDefaultTarget(objSelf).GetComponentInChildren<LookAtPlayerIKControl>().lookAwayOn = lookAway;
         //Fsm.GetOwnerDefaultTarget(objSelf).GetComponent<LookatPlayer>().enabled = lookAtPlayerOn;
 
 
